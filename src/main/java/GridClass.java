adda311cea98c9637808c51d73511e9674d1a6e5
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-08 14:53:00 +0700

    Added get size method. Test to get size was success

--- src/main/java/GridClass.java
+++ src/main/java/GridClass.java
@@ -1,3 +1,8 @@
 public class GridClass {
     GridClass() {}
+
+    public int[] getSize() {
+        //todo добавить поля для класса с размерами поля и выдать их
+        return new int[2];
+    }
 }
