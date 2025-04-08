9ea359c2b5782e90dee1124860799d6252252bd5
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-08 14:27:28 +0700

    Grid creation test

---
+++ src/test/java/GridTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class GridTest {
+
+    @Test
+    public void gridClassCreationTest(){
+        GridClass grid = new GridClass();
+        Assert.assertNotNull(grid);
+    }
+}
