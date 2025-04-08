b4dd2bca406b25d5fa18b5eedc4361e4ce5d66f7
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-08 16:24:10 +0700

    Added var grid and put the ship on the grid. Ship test was success

--- src/main/java/GridClass.java
+++ src/main/java/GridClass.java
@@ -2,12 +2,11 @@ public class GridClass {
     int width = 10;
     int height = 10;
 
-    GridClass() {}
+    private int[][] grid = new int[width][height];
+
+    GridClass() { grid[0][0] = 1; }//todo сделать метод для генерации кораблей
 
     public int[] getSize() { return new int[]{width, height}; }
 
-    public int[][] getGrid() {
-        //todo добавить переменную grid и возвращать уже её
-        return new int[][]{{width, height}};
-    }
+    public int[][] getGrid() { return grid;}
 }
