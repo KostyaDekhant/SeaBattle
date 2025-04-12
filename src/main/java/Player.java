ce582935e11c6d8e257662d92b62a04f29307cb9
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-12 18:53:25 +0700

    Added has lost method. Test was success

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -8,4 +8,15 @@ public class Player {
     public boolean makeMove(int x, int y, Player opponent) {
         return opponent.grid.shoot(x, y) != 1;
     }
+
+    public boolean hasLost() {
+        int[][] field = grid.getGrid();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (field[i][j] == 1) return false; // есть ещё живые палубы
+            }
+        }
+        return true;
+    }
+
 }
