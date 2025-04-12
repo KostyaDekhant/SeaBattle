a88e0da5840ee420f5f45d2c0ace9c10f60fa059
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-12 19:04:17 +0700

    Added ship is destroyed test

--- src/test/java/ShipTest.java
+++ src/test/java/ShipTest.java
@@ -10,4 +10,17 @@ public class ShipTest {
 
         Assert.assertEquals(2, ship.getSize());
     }
+
+    @Test
+    public void shipIsDestroyedTest() {
+        GridClass grid = new GridClass();
+        Ship ship = new Ship();
+        ship.addCell(2, 2);
+        ship.addCell(2, 3);
+
+        grid.getGrid()[2][2] = 2;
+        grid.getGrid()[2][3] = 2;
+
+        Assert.assertTrue(ship.isDestroyed(grid));
+    }
 }
