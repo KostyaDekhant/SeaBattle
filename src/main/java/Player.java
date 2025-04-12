7516018c59bfd9233be1d3f67549523046063f16
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-12 18:46:06 +0700

    Added make move method. Test was success

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,3 +1,11 @@
 public class Player {
-    Player(){}
+    private GridClass grid = new GridClass();
+
+    public GridClass getGrid() {
+        return grid;
+    }
+
+    public boolean makeMove(int x, int y, Player opponent) {
+        return opponent.grid.shoot(x, y) != 1;
+    }
 }
