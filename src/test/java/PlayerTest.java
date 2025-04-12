794487c83cf6d8ebd893ee98fe35b5f3263ba92f
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-12 18:53:02 +0700

    Added player loses after all ships destroyed test

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -16,4 +16,22 @@ public class PlayerTest {
         boolean result = player1.makeMove(0, 0, player2);
         Assert.assertTrue(result); // знаем, что на (0,0) есть корабль
     }
+
+    @Test
+    public void playerLosesAfterAllShipsDestroyed() {
+        Player player = new Player();
+        player.getGrid().generateShips();
+
+        // Уничтожаем все корабли
+        player.getGrid().shoot(0, 0);
+        player.getGrid().shoot(2, 2); player.getGrid().shoot(2, 3);
+
+        player.getGrid().shoot(5, 5); player.getGrid().shoot(6, 5);
+        player.getGrid().shoot(7, 5);
+
+        player.getGrid().shoot(8, 8); player.getGrid().shoot(8, 7);
+        player.getGrid().shoot(8, 6); player.getGrid().shoot(8, 5);
+
+        Assert.assertTrue(player.hasLost());
+    }
 }
