adf0dab62be899e4b22251ef3b83efa088eaa901
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-12 18:33:14 +0700

    Added player can make move test

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -6,4 +6,14 @@ public class PlayerTest {
         Player player = new Player();
         Assert.assertNotNull(player);
     }
+
+    @Test
+    public void playerCanMakeMoveTest() {
+        Player player1 = new Player();
+        Player player2 = new Player();
+        player2.getGrid().generateShips();
+
+        boolean result = player1.makeMove(0, 0, player2);
+        Assert.assertTrue(result); // знаем, что на (0,0) есть корабль
+    }
 }
