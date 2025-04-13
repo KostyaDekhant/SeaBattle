40fecef209bbc2a43ed8ef632b20de880e9e6750
KostyaDekhant <112933199+KostyaDekhant@users.noreply.github.com>
2025-04-13 13:02:39 +0700

    Added ships do not touch each other test

--- src/test/java/ShipTest.java
+++ src/test/java/ShipTest.java
@@ -37,4 +37,30 @@ public class ShipTest {
 
         Assert.assertEquals("Должно быть 10 кораблей на поле", 10, ships.size());
     }
+
+    @Test
+    public void shipsDoNotTouchEachOtherTest() {
+        Player player = new Player();
+        player.generateShips();
+        List<Ship> ships = player.getShips();
+
+        for (int i = 0; i < ships.size(); i++) {
+            for (int j = i + 1; j < ships.size(); j++) {
+                List<int[]> shipA = ships.get(i).getCells();
+                List<int[]> shipB = ships.get(j).getCells();
+
+                for (int[] a : shipA) {
+                    for (int[] b : shipB) {
+                        if (areTouching(a, b)) {
+                            Assert.fail("Корабли соприкасаются: (" + a[0] + " , " + a[1] + ") и (" + b[0] + " , " + b[1] + ")");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean areTouching(int[] a, int[] b) {
+        return Math.abs(a[0] - b[0]) <= 1 && Math.abs(a[1] - b[1]) <= 1;
+    }
 }
